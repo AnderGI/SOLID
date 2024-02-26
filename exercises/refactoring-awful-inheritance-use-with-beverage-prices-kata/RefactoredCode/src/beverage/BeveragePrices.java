278282582244c22d89be9a25ee3ffc4137169c71
AnderGI <agibarguen@gmail.com>
2024-02-26 10:08:37 +0100

    Beverage Prices constants class added

---
+++ exercises/refactoring-awful-inheritance-use-with-beverage-prices-kata/RefactoredCode/src/beverage/BeveragePrices.java
@@ -0,0 +1,7 @@
+package beverage;
+
+final public class BeveragePrices {
+	public static final Double TEA = 1.5;
+	public static final Double HOT_COCOLATE = 1.45;
+	public static final Double COFFE = 1.2;
+}
